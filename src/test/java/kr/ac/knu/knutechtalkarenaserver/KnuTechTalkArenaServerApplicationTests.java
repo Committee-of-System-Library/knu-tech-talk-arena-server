e1069c95a84d510e8574e7bf36af175587000e04
wlgns12370 <knu12370@knu.ac.kr>
2025-09-19 15:59:08 +0900

    init: knu-tech-talk-arena 초기 개발환경 구성

---
+++ src/test/java/kr/ac/knu/knutechtalkarenaserver/KnuTechTalkArenaServerApplicationTests.java
@@ -0,0 +1,9 @@
+package kr.ac.knu.knutechtalkarenaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnuTechTalkArenaServerApplicationTests {
+
+}
